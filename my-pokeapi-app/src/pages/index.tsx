23984bb5afc2ed6ff88bbe123743b9b8c8d1389d
michael wahyudin <michaelwahyudin90@gmail.com>
2024-01-30 11:43:48 +0700

    ssr

--- my-pokeapi-app/src/pages/index.tsx
+++ my-pokeapi-app/src/pages/index.tsx
@@ -1,7 +1,6 @@
 // pages/index.tsx
 import { useEffect, useState } from 'react';
 import getPokemonList from './api/pokeApi';
-import Image from 'next/image';
 
 interface PokemonWithSprite {
   name: string;
@@ -9,34 +8,33 @@ interface PokemonWithSprite {
   sprite: string;
 }
 
-export default function Home() {
-  const [pokemonList, setPokemonList] = useState<PokemonWithSprite[]>([]);
-
-  useEffect(() => {
-    const fetchPokemonList = async () => {
-      const list = await getPokemonList();
-      setPokemonList(list);
-    };
-
-    fetchPokemonList();
-  }, []);
-
+export default function Home({ pokemonList }: { pokemonList: PokemonWithSprite[] }) {
   return (
     <div>
-      <h1>Pokemon List</h1>
+      <h1 className="title">Pokemon List</h1>
       <ul>
         {pokemonList.map((pokemon) => (
           <li key={pokemon.name}>
-            <Image
-              src={pokemon.sprite}
-              alt={pokemon.name}
-              width={50} // set the width of the image as needed
-              height={50} // set the height of the image as needed
-            />
+            <img src={pokemon.sprite} alt={pokemon.name} />
             {pokemon.name}
           </li>
         ))}
       </ul>
+
+      <style jsx>{`
+        .title {
+          color: blue;
+        }
+
+        /* Other styles */
+      `}</style>
     </div>
   );
+}
+
+export async function getServerSideProps() {
+  const pokemonList = await getPokemonList();
+  return {
+    props: { pokemonList },
+  };
 }
